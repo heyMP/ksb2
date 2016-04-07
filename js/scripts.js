46dff7004567f79409b078e7a52cbb4e95ec0a8e
heyMP <mgp140@gmail.com>
2016-04-07 16:12:32 -0400

    Removed html5mode :(  Can't do it with gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,6 @@ var angularRoute = require('angular-route');
     'ngRoute'
   ])
   .config(function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
 
     $routeProvider
     .when('/', {
